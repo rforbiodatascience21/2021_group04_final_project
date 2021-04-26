410e5fb7a02988d292e2812664d34e2e11f31532
macviu <macviu@dtu.dk>
2021-04-26 12:59:15 +0000

    Added parent and last population + T cell type

--- R/03_augment.R
+++ R/03_augment.R
@@ -18,21 +18,14 @@ covid_data_clean <- read_tsv(file = "data/02_covid_data_clean.tsv")
 covid_data_augment <- covid_data_clean %>% 
   pivot_longer(cols = contains("lymphocytes"),
                names_to = "Gating",
-               values_to = "Fraction")
+               values_to = "Fraction") %>% 
+  mutate(T_cell = case_when(str_detect(Gating, "CD8") ~ "CD8",
+                            str_detect(Gating, "CD4") ~ "CD4"),
+         Parent_population = str_match(Gating, "\\S+\\/(\\S+)\\/\\S+$")[,2],
+         Last_population = str_match(Gating, "\\S+\\/(\\S+)$")[,2],
+         .before = Fraction)
 
 
-covid_data_augment <- covid_data_augment %>% 
-  separate(col = Gating,
-           into = c("Lymphocytes", "Singlets", "Live_cells", "CD3",
-                    "CD8", "Multimers", "Marker"),
-           sep = "/",
-           remove = FALSE) %>% 
-  select(-c(Lymphocytes, Singlets, Live_cells, CD3, CD8))
-
-covid_data_augment %>% 
-  unite
-
-?coalesce()
 
 
 # Write data --------------------------------------------------------------
