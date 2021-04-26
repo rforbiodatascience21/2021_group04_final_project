51f93939af8c3ac2fbbebdf443c6f089d45f4f9d
skosk-dtu <skosk@dtu.dk>
2021-04-26 08:57:39 +0000

    reading and writing data in 01_load

--- R/01_load.R
+++ R/01_load.R
@@ -4,6 +4,7 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("readxl")
 
 
 # Define functions --------------------------------------------------------
@@ -11,7 +12,7 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_raw <- read_tsv(file = "data/_raw/my_raw_data.tsv")
+covid_data_raw <- read_excel("data/_raw/covid_data.xlsx")
 
 
 # Wrangle data ------------------------------------------------------------
@@ -19,5 +20,5 @@ my_data <- my_data_raw # %>% ...
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data,
-          file = "data/01_my_data.tsv")
+write_tsv(x = covid_data_raw,
+          file = "data/01_covid_data.tsv")
