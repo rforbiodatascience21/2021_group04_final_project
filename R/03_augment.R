e7e3d037859ecb03baee4f6bfce22574d780eeb9
skosk-dtu <skosk@dtu.dk>
2021-04-26 10:19:11 +0000

    agumenting covid_data_clean

--- R/03_augment.R
+++ R/03_augment.R
@@ -11,11 +11,28 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean <- read_tsv(file = "data/02_my_data_clean.tsv")
+covid_data_clean <- read_tsv(file = "data/02_covid_data_clean.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug <- my_data_clean # %>% ...
+covid_data_augment <- covid_data_clean %>% 
+  pivot_longer(cols = contains("lymphocytes"),
+               names_to = "Gating",
+               values_to = "Fraction")
+
+
+covid_data_augment <- covid_data_augment %>% 
+  separate(col = Gating,
+           into = c("Lymphocytes", "Singlets", "Live_cells", "CD3",
+                    "CD8", "Multimers", "Marker"),
+           sep = "/",
+           remove = FALSE) %>% 
+  select(-c(Lymphocytes, Singlets, Live_cells, CD3, CD8))
+
+covid_data_augment %>% 
+  unite
+
+?coalesce()
 
 
 # Write data --------------------------------------------------------------
