18b88d491c90df306b73b29c51276059961930bf
skosk-dtu <skosk@dtu.dk>
2021-04-26 10:18:22 +0000

    cleaining covid_data

--- R/02_clean.R
+++ R/02_clean.R
@@ -11,13 +11,24 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
+covid_data <- read_tsv(file = "data/01_covid_data.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+covid_data_clean <- covid_data %>% 
+  na_if("NA") %>% 
+  mutate_if(is.numeric, round, digits=1)
+
+covid_data_clean <- covid_data %>% 
+  mutate(cohort_type = case_when( str_detect(SampleID, "Patient") ~ "Patient",
+                                  str_detect(SampleID, "HD-1") ~ "HD-1",
+                                  str_detect(SampleID, "HD-2") ~"HD-2"),
+         .after = SampleID,
+         cohort_type = factor(cohort_type, 
+                              levels = c("Patient", "HD-1", "HD-2"))
+         )
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data_clean,
-          file = "data/02_my_data_clean.tsv")
+write_tsv(x = covid_data_clean,
+          file = "data/02_covid_data_clean.tsv")
