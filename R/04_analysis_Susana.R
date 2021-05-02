c6b2fc4d70768a9dfb9b00e4afe31ef60fe8f711
supaah <supaah@dtu.dk>
2021-05-02 19:37:12 +0000

    New graph

--- R/04_analysis_Susana.R
+++ R/04_analysis_Susana.R
@@ -57,6 +57,24 @@ covid_data_augment %>%
                  strip.background = element_rect(colour="white")
            )
 
+covid_data_augment %>% 
+  filter(Last_population =="SARS_multimer+") %>% filter(!is.na (Fraction)) %>%
+  ggplot(mapping = aes(x=Hospital_status, y = Fraction, color = Hospital_status)
+  )+
+  geom_boxplot(outlier.shape=1, outlier.size=2)+
+  stat_boxplot(geom='errorbar', linetype=1, width=0.2)+
+  stat_summary(fun = mean, geom="point", size=3)+
+  geom_jitter(size=2, shape=1, position=position_jitter(0.0))+
+  labs(title="Frequencies of SARS-CoV-2 multimer+ CD8+ T cells in outpatient and hospitalized patients", 
+       y = ("SARS-CoV-2 specific T cells \n (% of multimer+ CD8+ Tcells)"))+
+  theme_bw()+
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        legend.title = element_blank(),
+        axis.title.x = element_blank()
+  )
+
+
 
 # Write data --------------------------------------------------------------
 write_tsv(...)
