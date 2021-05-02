fd40b84d9a9d4faa2132d194abacff9ebd4d5576
skosk-dtu <skosk@dtu.dk>
2021-05-02 16:04:13 +0000

    ggplot in progress

--- R/04_analysis_Signe.R
+++ R/04_analysis_Signe.R
@@ -11,15 +11,30 @@ source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data_clean_aug <- read_tsv(file = "data/03_my_data_clean_aug.tsv")
+covid_data_augment <- read_tsv(file = "data/03_covid_data_augment.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
+
+  
+#Here you can assign your values of
+
+covid_data_augment %>% 
+  pivot_wider(names_from = SampleID,
+              values_from = Fraction) %>% 
+  select(-c(Patient_19:Patient_42)) 
+
 
 
 # Model data
-my_data_clean_aug %>% ...
+covid_data_augment %>% 
+  filter(Last_population == c("CD38", "CD39"),
+         SampleID == c("Patient_1")) %>% 
+  ggplot(aes(x = Last_population,
+             y = Fraction,
+             fill = cohort_type))+
+  geom_boxplot()+
+  geom_point(position = position_dodge(0.75))
 
 
 # Visualise data ----------------------------------------------------------
