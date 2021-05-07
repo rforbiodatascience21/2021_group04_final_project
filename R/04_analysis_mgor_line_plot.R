b0e9580cf25b33edb5bd60c1448cfe3413d7bbf6
mgor95 <mgor@dtu.dk>
2021-05-07 09:44:15 +0000

    with facet wrap function

--- R/04_analysis_mgor_line_plot.R
+++ R/04_analysis_mgor_line_plot.R
@@ -3,6 +3,25 @@
 
 #---- this does a facetwrap, of selected group, however, has to figure out how to clean it from graphs which don't has CEF expression
 
+#FACETWRAP
+covid_data_augment %>% 
+  filter(
+    cohort_type == "Patient",
+    str_detect(Parent_population, "multimer+")) %>%
+  ggplot(
+    aes(
+      x = Parent_population,
+      y = Fraction))+
+  geom_point(color = "dimgray")+
+  geom_line(aes(group = SampleID),
+            color = "dimgray")+
+  theme(legend.position = "none")+
+  facet_wrap(vars(Last_population),
+             scales = "free_y")+
+  theme_classic()
+
+#--------
+
 covid_data_augment %>% 
   filter(
     cohort_type == "Patient",
