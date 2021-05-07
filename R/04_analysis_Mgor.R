e3cdbb53ceb5c24efedfd2975df5f2dcdc7775f4
mgor95 <mgor@dtu.dk>
2021-05-07 09:54:14 +0000

    line plots

--- R/04_analysis_Mgor.R
+++ R/04_analysis_Mgor.R
@@ -7,6 +7,9 @@ library(tidyverse)
 library(dplyr)
 library(ggpubr)
 
+install.packages("patchwork")
+library(patchwork)
+
 ## idea is to create a function, which will generate graph/graphs
 ## depending what input I want. 
 ## so lets say I have the full dataframe, I filter on CD38, 
@@ -52,7 +55,7 @@ test_nested <-
    
 
 
-covid_data_augment_TEST %>% 
+covid_data_augment %>% 
   filter(Parent_population == "SARS_multimer",
          Last_population == "CD38") %>%
   ggplot(mapping = aes(
@@ -64,11 +67,6 @@ covid_data_augment_TEST %>%
   geom_boxplot(outlier.shape = NA,
                fill = NA,
                color = "gray")+
-  stat_compare_means(method = "wilcox.test", comparisons = 
-                       list(c("HD-1", "HD-2"),
-                            c("HD-2", "Patient"),
-                            c("HD-1", "Patient")),
-                     hide.ns = TRUE)+
   theme_classic()
   
 
@@ -162,3 +160,146 @@ covid_data_augment %>%
   filter(Parent_population == "SARS_multimer+",
          Last_population == "CD38") %>%
   View()
+
+
+
+
+#---------SIGNES VERSION---------
+
+covid_data_augment %>% 
+  filter(cohort_type == "Patient",
+         Last_population == c("CD38", "PD1")) %>% 
+  ggplot(aes(x = Last_population,
+             y = Fraction,
+             fill = Parent_population))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_dotplot(binaxis = "y",
+               stackdir = "center",
+               position = position_dodge(0.75))+
+  theme_classic()+
+  theme(legend.position = "bottom")+
+  labs(x = "",
+       y = "% of multimer+ CD8+ T cells",
+       fill = "")+
+  scale_fill_discrete(labels = c("CEF", "SARS-CoV-2"))
+
+
+##----Testing to do my own facet_wrap
+pops <- c("CD38", "PD1")
+
+covid_data_augment %>% 
+  filter(Last_population %in% c("CD27",
+                                "CD38",
+                                "CD39",
+                                "CD57",
+                                "CD69",
+                                "HLA-DR",
+                                "PD-1"),
+         str_detect(Parent_population, "SARS")) %>% 
+  ggplot(aes(x = cohort_type,
+             y = Fraction,
+             color = cohort_type))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_dotplot(binaxis = "y",
+               stackdir = "center",
+              dotsize = 0.3,
+              na.rm = TRUE)+
+  facet_wrap(vars(Last_population),
+             nrow = 1)+
+  theme_classic()+
+  geom_signif(comparisons = list(c("HD-1", "Patient"),
+                                 c("HD-2", "Patient")),
+              method = "kruskal.test",
+              map_signif_level = TRUE,
+              margin_top = 0.005,
+              step_increase = 0.1,
+              vjust = 0.7,
+              textsize = 3)
+
+
+
+covid_data_augment_wider_SARS <- covid_data_augment %>% 
+  select(-Gating) %>% 
+  filter(Parent_population == "SARS_multimer+") %>% 
+  pivot_wider(names_from = Last_population,
+              values_from = Fraction)
+
+covid_data_augment_wider_SARS %>% View()
+
+
+covid_data_augment_wider_SARS %>%
+  ggplot(aes(
+    x = CD38,
+    y = CD39,
+    color = cohort_type))+
+  geom_point()+
+  geom_smooth(method = lm,
+              color = "dimgray")+
+  theme_classic()
+
+#---- line plot
+
+covid_data_augment %>% 
+  filter(
+    cohort_type == "Patient",
+    str_detect(Parent_population, "multimer+"),
+    Last_population == "CD38") %>%
+  ggplot(
+    aes(
+      x = Parent_population,
+      y = Fraction
+    ))+
+  geom_point()+
+  geom_line(aes(group = SampleID))+
+  theme(legend.position = "none")
+
+#-----Facet wrapping line plot-----
+
+covid_data_augment %>% 
+  filter(Last_population %in% c("CD27", "CD38", "CD39", "CD57", "CD69", "HLA-DR", "PD-1"),
+    cohort_type == "Patient",
+    str_detect(Parent_population, "multimer+")) %>%
+  ggplot(
+    aes(
+      x = Parent_population,
+      y = Fraction))+
+  geom_point(color = "dimgray")+
+  geom_line(aes(group = SampleID),
+            color = "dimgray")+
+  theme(legend.position = "none")+
+ facet_wrap(vars(Last_population),
+            scales = "free_y")+
+  theme_classic()
+
+#---- Picking the ones we have data of-----
+
+covid_data_augment %>% 
+  filter(
+    cohort_type == "Patient",
+    str_detect(Parent_population, "multimer+"),
+    Last_population == "CD27") %>%
+  ggplot(
+    aes(
+      x = Parent_population,
+      y = Fraction))+
+  geom_point(color = "dimgray")+
+  geom_line(aes(group = SampleID),
+            color = "dimgray")+
+  theme(legend.position = "none")
+
+CD38_line <- covid_data_augment %>% 
+  filter(
+    cohort_type == "Patient",
+    str_detect(Parent_population, "multimer+"),
+    Last_population == "CD38") %>%
+  ggplot(
+    aes(
+      x = Parent_population,
+      y = Fraction))+
+  geom_point(color = "dimgray")+
+  geom_line(aes(group = SampleID),
+            color = "dimgray")+
+  theme(legend.position = "none")
+
+CD27_line + CD38_line
+  
