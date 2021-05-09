1af332e7737cf64ae931b47d0e539faca4810f2a
supaah <you@example.com>
2021-05-09 10:27:42 +0000

    change

--- R/01_load.R
+++ R/01_load.R
@@ -11,10 +11,7 @@ library("readxl")
 covid_data_raw <- read_excel("data/_raw/covid_data.xlsx")
 
 
-# Wrangle data ------------------------------------------------------------
-my_data <- my_data_raw # %>% ...
-
-
 # Write data --------------------------------------------------------------
 write_tsv(x = covid_data_raw,
           file = "data/01_covid_data.tsv")
+
