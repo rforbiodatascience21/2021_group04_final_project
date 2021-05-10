5687d6a00ae7c3418844a74ae95aede2fb2c9bf8
Signe Koggersbøl Skadborg <skosk@dtu.dk>
2021-05-10 12:05:45 +0200

    merging doit scripts

--- R/00_doit.R
+++ R/00_doit.R
@@ -4,3 +4,8 @@ source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
 source(file = "R/04_analysis_i.R")
 
+source(file = "R/01_load_FCS.R")
+source(file = "R/02_clean_FCS.R")
+source(file = "R/03_augment_FCS.R")
+source(file = "R/04_analysis_FCS_i.R")
+
--- R/00_doit_FCS.R
+++
@@ -1,6 +0,0 @@
-# Run all scripts ---------------------------------------------------------
-source(file = "R/01_load_FCS.R")
-source(file = "R/02_clean_FCS.R")
-source(file = "R/03_augment_FCS.R")
-source(file = "R/04_analysis_FCS_i.R")
-
